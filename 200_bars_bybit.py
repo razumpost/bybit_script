58f2de5bffdeb880510b12f190a37b49add6459f
razumpost <razumpost@gmail.com>
2023-12-15 16:19:12 +0300

    one column about BTC

--- 200_bars_bybit.py
+++ 200_bars_bybit.py
@@ -6,90 +6,6 @@ import time
 import numpy as np
 
 
-
-def kline(symb, tf):
-    url = "https://api.bybit.com"
-    path = "/v5/market/kline"
-    URL = url + path
-    params = {'category': 'linear', "symbol": symb, "interval": tf}
-    r = requests.get(URL, params=params)
-    df = pd.DataFrame(r.json()['result']['list'])
-    # pd.set_option('max_columns', None)
-    m = pd.DataFrame()
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=FutureWarning)
-        m['Date'] = pd.to_datetime(df.iloc[:, 0], unit="ms")
-        m['Open'] = df.iloc[:, 1].astype(float)
-        m['High'] = df.iloc[:, 2].astype(float)
-        m['Low'] = df.iloc[:, 3].astype(float)
-        m['Close'] = df.iloc[:, 4].astype(float)
-        m['Volume'] = df.iloc[:, 5].astype(float)
-        m = m.sort_values(by='Date', ignore_index=True)
-
-    return m
-
-
-def kline2(symb, tf, start='2023-12-12 00:00'):
-    url = "https://api.bybit.com"
-    path = "/v5/market/kline"
-    URL = url + path
-    start_ds = dt.datetime.strptime(start, '%Y-%m-%d %H:%M')
-    start_ds = int(start_ds.timestamp()*1000)
-    end_ds = int(time.time()*1000)
-    params = {'category': 'linear', "symbol": symb, "interval": tf, 'start': start_ds, 'end': end_ds}
-    r = requests.get(URL, params=params)
-    df = pd.DataFrame(r.json()['result']['list'])
-    # pd.set_option('max_columns', None)
-    m = pd.DataFrame()
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=FutureWarning)
-        m['Date'] = pd.to_datetime(df.iloc[:, 0], unit="ms")
-        m['Open'] = df.iloc[:, 1].astype(float)
-        m['High'] = df.iloc[:, 2].astype(float)
-        m['Low'] = df.iloc[:, 3].astype(float)
-        m['Close'] = df.iloc[:, 4].astype(float)
-        m['Volume'] = df.iloc[:, 5].astype(float)
-        m = m.sort_values(by='Date', ignore_index=True)
-    return m
-
-
-def kline3(symb, tf, start='2023-12-12 00:00'):
-
-    url = "https://api.bybit.com"
-    path = "/v5/market/kline"
-    URL = url + path
-    start_ds = dt.datetime.strptime(start, '%Y-%m-%d %H:%M')
-    start_ds = int(start_ds.timestamp()*1000)
-    end_ds = int(time.time()*1000)
-
-    batch_size = 200
-    dtf = int(tf) * 60 * 1000
-    N = round(((end_ds - start_ds)/dtf) + 0.5)
-    batch_count = N // batch_size + int(N % batch_size != 0)
-
-    dfs = pd.DataFrame()
-    for i in range(batch_count):
-        batch_start = start_ds + i * batch_size*dtf
-        batch_end = min(end_ds, batch_start + batch_size*dtf)
-
-        params = {'category': 'linear', "symbol": symb, "interval": tf, 'start': batch_start, 'end': batch_end}
-        r = requests.get(URL, params=params)
-        df = pd.DataFrame(r.json()['result']['list'])
-        # pd.set_option('max_columns', None)
-        m = pd.DataFrame()
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", category=FutureWarning)
-            m['Date'] = pd.to_datetime(df.iloc[:, 0], unit="ms")
-            m['Open'] = df.iloc[:, 1].astype(float)
-            m['High'] = df.iloc[:, 2].astype(float)
-            m['Low'] = df.iloc[:, 3].astype(float)
-            m['Close'] = df.iloc[:, 4].astype(float)
-            m['Volume'] = df.iloc[:, 5].astype(float)
-            m = m.sort_values(by='Date', ignore_index=True)
-
-        dfs = pd.concat([dfs, m])
-    return dfs
-
 def kline4(symb, tf, N):
 
     url = "https://api.bybit.com"
@@ -122,11 +38,15 @@ def kline4(symb, tf, N):
             m['Open'] = df.iloc[:, 1].astype(float)
             m['High'] = df.iloc[:, 2].astype(float)
             m['Low'] = df.iloc[:, 3].astype(float)
-            m['Close'] = df.iloc[:, 4].astype(float)
+            m['Close_BTC'] = df.iloc[:, 4].astype(float)
             m['Volume'] = df.iloc[:, 5].astype(float)
             m = m.sort_values(by='Date')
 
         dfs = pd.concat([dfs, m], ignore_index=True)
     return dfs
 
-print(kline4("BTCUSDT", 5, 1500))
+
+result = kline4('BTCUSDT', 5, 1500)
+close_column = result['Close_BTC']
+
+print(close_column)
